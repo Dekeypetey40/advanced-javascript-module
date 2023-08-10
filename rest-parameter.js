f4dff974c14f5a826dfa3eca0422ec594bcde678
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-08-10 14:13:32 +0200

    Learn rest parameter

--- rest-parameter.js
+++ rest-parameter.js
@@ -1,7 +1,20 @@
 
 
 // Regular function call 
-
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3); //Gives 6
+console.log(sum)
 // Extra arguments are ignored
-
+let sum2 = sumAll(1, 2, 3, 4, 5, 6); //Gives same result as only 3 parameters
+console.log(sum2)
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6)
+console.log(sum3) // This gives 21 so it picks up all parameters passed
